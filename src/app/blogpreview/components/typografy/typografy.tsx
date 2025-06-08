0f3c7126d14a4ac0fda27163e9951955b951b7ce
Dylorena <dilorenasantos@gmail.com>
2025-06-07 22:48:22 -0300

    feat: mobile view

--- src/app/blogpreview/components/typografy/typografy.tsx
+++ src/app/blogpreview/components/typografy/typografy.tsx
@@ -1,10 +1,23 @@
-
-type Props = {
+type Props<T extends React.ElementType> = {
+  as?: T;
   variant: "preset-1" | "preset-2" | "preset-3" | "preset-3-b";
+  color?: any;
   children: React.ReactNode;
-};
+  className?: any;
+} & React.ComponentPropsWithoutRef<T>;
 
-export function Typografy({ variant, children }: Props) {
-  return <span
-  className={`text-${variant}`}>{children}</span>;
+export function Typografy<T extends React.ElementType = "span">({
+  as,
+  variant,
+  children,
+  color = "text-gray-950",
+  className,
+  ...rest
+}: Props<T>) {
+  const Component = as || "span";
+  return (
+    <Component className={`text-${variant} ${color} ${className}`} {...rest}>
+      {children}
+    </Component>
+  );
 }
--- src/app/blogpreview/page.tsx
+++ src/app/blogpreview/page.tsx
@@ -1,4 +1,6 @@
 import { Figtree } from "next/font/google";
+import { Typografy } from "./components/typografy/typografy";
+import Image from "next/image";
 
 const figtree = Figtree({
   subsets: ["latin"],
@@ -8,17 +10,49 @@ const figtree = Figtree({
 
 export default function BlogPreview() {
   return (
-    <div className={`${figtree.className} bg-yellow`} >
-      Learning Published 21 Dec 2023 HTML & CSS foundations These languages are
-      the backbone of every website, defining structure, content, and
-      presentation. Greg Hooper
-      <div className="attribution">
-        Challenge by{" "}
-        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
-          Frontend Mentor
-        </a>
-        . Coded by <a href="#">Your Name Here</a>.
-      </div>
+    <div
+      className={`${figtree.className} bg-yellow h-[100vh] flex items-center justify-center`}
+    >
+      <main className="bg-white w-[20.4375rem] h-[31.3125rem] p-6 rounded-[20px] ring-1 ring-inset flex flex-col gap-6 lg:w-[24rem] lg:h-[32.625rem] shadow-[8px_8px_0_#000]">
+        <Image
+          src="/blogpreview-images/illustration-article.svg"
+          priority
+          alt="image mix yellow with black color"
+          width="279"
+          height="200"
+          className="rounded-[0.625rem]"
+        />
+        <section className="flex flex-col gap-y-3">
+          <Typografy
+            as="p"
+            variant="preset-3-b"
+            className="bg-yellow mb-3 w-[4.5625rem] h-[1.625rem] rounded-sm text-center content-center"
+          >
+            Learning
+          </Typografy>
+
+          <Typografy as="time" variant="preset-3">Published 21 Dec 2023 </Typografy>
+          <Typografy as="h1" variant="preset-1" className="hover:text-yellow">
+            HTML & CSS foundations
+          </Typografy>
+          <Typografy as="p" variant="preset-2" color="text-gray-500">
+            These languages are the backbone of every website, defining
+            structure, content, and presentation.
+          </Typografy>
+        </section>
+
+        <div className="flex gap-3">
+          <Image
+            src="/blogpreview-images/image-avatar.webp"
+            width={32}
+            height={32}
+            alt="Greg Hooper"
+          />
+          <Typografy as="cite" variant="preset-3-b" className="self-center not-italic">
+            Greg Hooper
+          </Typografy>
+        </div>
+      </main>
     </div>
   );
 }
