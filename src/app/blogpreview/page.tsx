0b471520c310af7527426709f0cb431708e9c886
Dylorena <dilorenasantos@gmail.com>
2025-06-10 22:27:46 -0300

    feat: adds desktop adjustments

--- src/app/blogpreview/page.tsx
+++ src/app/blogpreview/page.tsx
@@ -13,20 +13,20 @@ export default function BlogPreview() {
     <div
       className={`${figtree.className} bg-yellow h-[100vh] flex items-center justify-center`}
     >
-      <main className="bg-white w-[20.4375rem] h-[31.3125rem] p-6 rounded-[20px] ring-1 ring-inset flex flex-col gap-6 lg:w-[24rem] lg:h-[32.625rem] shadow-[8px_8px_0_#000]">
+      <main className="bg-white w-[20.4375rem] h-[31.3125rem] p-6 rounded-[20px] ring-1 ring-inset flex flex-col justify-between lg:w-[24rem] lg:h-[32.625rem] shadow-[8px_8px_0_#000]">
         <Image
           src="/blogpreview-images/illustration-article.svg"
           priority
           alt="image mix yellow with black color"
           width="279"
           height="200"
-          className="rounded-[0.625rem]"
+          className="rounded-[0.625rem] lg:w-[21rem]"
         />
         <section className="flex flex-col gap-y-3">
           <Typografy
             as="p"
             variant="preset-3-b"
-            className="bg-yellow mb-3 w-[4.5625rem] h-[1.625rem] rounded-sm text-center content-center"
+            className="bg-yellow w-[4.5625rem] h-[1.625rem] lg:w-[5.125rem] lg:h-[1.8125rem] rounded-sm text-center content-center"
           >
             Learning
           </Typografy>
